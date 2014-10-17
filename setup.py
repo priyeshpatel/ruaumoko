bcd0030929ece805f3b1df95ecb0148f29785663
Rich Wareham <rjw57@cam.ac.uk>
2014-10-17 14:21:18 +0100

    setup.py: enable ruaumoko-download for all Pythons
    With 42c984b, ruaumoko-download now works on Python 2 as well.

--- setup.py
+++ setup.py
@@ -46,12 +46,9 @@ def get_version():
 
 console_scripts = [
     "ruaumoko-api = ruaumoko.api:main",
-    "ruaumoko-get = ruaumoko.get_cmd:main"
+    "ruaumoko-get = ruaumoko.get_cmd:main",
+    "ruaumoko-download = ruaumoko.download:main",
 ]
-
-PY2 = sys.version_info[0] == 2
-if not PY2:
-    console_scripts.append("ruaumoko-download = ruaumoko.download:main")
 
 setup(
     name="Ruaumoko",
