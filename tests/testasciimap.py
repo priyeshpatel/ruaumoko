984ab7234de7cf8ad675174b4b8b7ac774d2c53b
Rich Wareham <rjw57@cam.ac.uk>
2014-10-23 11:55:20 +0100

    tests:asciimap: fix test on Python 3
    Patching print() is really rather subtle. Instead of patching print
    directly, patch sys.stdio.write.

--- tests/testasciimap.py
+++ tests/testasciimap.py
@@ -37,11 +37,20 @@ class TestASCIIMap(DownloadedDatasetTestCase):
     def test_simple_output(self):
         argv_patch = patch('sys.argv', [
             'ruaumoko-ascii-map', '-s', '60x20', '--tile-shape', '20x10', self.dataset_path])
-        print_patch = patch('__builtin__.print')
+
+        # Patch sys.stdout.write
+        stdout_write_patch = patch('sys.stdout.write')
 
         # Call the ascii map generator main function
-        with argv_patch, print_patch as mock_print:
+        with argv_patch, stdout_write_patch as mock_write:
             raa.main()
-            self.assertEqual(mock_print.call_count, len(expected_output_60_20))
-            mock_print.assert_has_calls(list(call(x) for x in expected_output_60_20), any_order=False)
+            captured_output = ''.join(args[0] for args, kwargs in mock_write.call_args_list)
+            captured_output = captured_output.splitlines()
+
+        # Check that the right number of lines were output
+        self.assertEqual(len(captured_output), len(expected_output_60_20))
+
+        # Check each line
+        for co_line, eo_line in zip(captured_output, expected_output_60_20):
+            self.assertEqual(co_line, eo_line)
 
