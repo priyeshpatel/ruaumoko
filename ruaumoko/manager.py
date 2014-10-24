dd3e1934bdbff86049a331927efd9c2eb02a3232
Rich Wareham <rjw57@cam.ac.uk>
2014-10-24 10:27:38 +0100

    manager: add configuration from environment
    For the development web server the environment variable
    ``RUAUMOKO_SETTINGS`` may now optionally be set. If it is set, it points
    to a configuration file which can be used to configure the webapp
    settings including, but not limited to, the path to the ruaumoko
    dataset.

--- ruaumoko/manager.py
+++ ruaumoko/manager.py
@@ -1,4 +1,5 @@
 """Command-line utility to run/manage webapp."""
+import os
 
 from flask.ext.script import Manager
 from .api import app
@@ -6,4 +7,6 @@ from .api import app
 manager = Manager(app)
 
 def main():
+    if 'RUAUMOKO_SETTINGS' in os.environ:
+        app.config.from_envvar('RUAUMOKO_SETTINGS')
     manager.run()
